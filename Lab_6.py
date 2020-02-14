1943303e9ac715a6cf9e4f3c594656ab0fce73aa
madisonli19 <60743988+madisonli19@users.noreply.github.com>
2020-02-14 18:16:11 -0500

    Update Lab_6.py

--- Lab_6.py
+++ Lab_6.py
@@ -1,4 +1,4 @@
 name = input(prompt="hey, whats your name?"))
-loation = input(prompt="hey, whats your name?"))
+loation = input(prompt="where are you from?"))
 print("so "+name+" you're from "+location+", i always hated it there.")
 print("i edited on jupyterhub.")
